e74d970aebfb1597f46f2aa77ded08a499aed9fe
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-06-03 01:59:48 +0530

    Added the employee payroll service in service layer

---
+++ src/main/java/com/bridgelabz/employeepayroll/service/IEmployeePayrollService.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.employeepayroll.service;
+
+import com.bridgelabz.employeepayroll.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayroll.model.EmployeePayrollData;
+
+import java.util.List;
+
+public interface IEmployeePayrollService {
+
+    List<EmployeePayrollData> getEmployeePayrollData();
+
+    EmployeePayrollData getEmployeePayrollDataById(long empId);
+
+    EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO);
+
+    EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO);
+
+    void deleteEmployeePayrollData(long empId);
+
+}
