b53d7c63d2e127ecbc29582dc5285b16f928ef34
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-06-06 00:54:54 +0530

    Added the validation regex for employee name and minimum salary

--- src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
@@ -1,7 +1,16 @@
 package com.bridgelabz.employeepayroll.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+
+    @Pattern(regexp = "[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name is Invalid")
+
+    @NotEmpty(message = "Employee name can not be null")
     public String name;
+    @Min(value = 500, message = "Minimum wage should be more tha 500")
     public long salary;
 
     public EmployeePayrollDTO(String name, long salary){
