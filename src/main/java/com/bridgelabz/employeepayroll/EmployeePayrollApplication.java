3a6ca923fb704bfc138da7a781802c63c736c83f
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-06-07 00:09:00 +0530

    Added the lombok library for logging

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollApplication.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.employeepayroll;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,10 +11,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollApplication.class, args);
+        log.info("Employee payroll App Started");
 	}
 	@Bean
 	public Docket productApi() {
